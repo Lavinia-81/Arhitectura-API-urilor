086a92ca700755fe5945c8ac8b9116b23d9a5317
Lavinia <marialavinia801@yahoo.com>
2026-07-12 13:44:39 +0100

    Removed old API folders and kept only poezii-api-example

--- poezii-api-clean/src/routes/v1/apiKey.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/v1/apiKey.ts
-
-import { FastifyInstance } from "fastify"
-import { createApiKeyController } from "../../controllers/apiKey.controller.js"
-
-export async function apiKeyRoutes(fastify: FastifyInstance) {
-  fastify.post("/api-keys", createApiKeyController)
-}
